a7d732e9a04f373d7dd095fc769809d979000ccd
Christopher Finke <cfinke@gmail.com>
2010-11-15 02:39:21 +0000

    Removing .git files when building

--- build-xpi.sh
+++ build-xpi.sh
@@ -7,7 +7,7 @@ chmod -R 0777 opmlsupport/
 
 mkdir .tmp_xpi_dir/
 cp -r opmlsupport/* .tmp_xpi_dir/
-rm -rf `find .tmp_xpi_dir/ -name ".svn"`
+rm -rf `find .tmp_xpi_dir/ -name ".git"`
 
 cd .tmp_xpi_dir/chrome/
 zip -rq ../opmlsupport.jar *
