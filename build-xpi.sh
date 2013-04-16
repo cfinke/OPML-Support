fc044370ecfb6f9eacb792c8417cc7e67d13db24
Christopher Finke <cfinke@Mahalo.local>
2013-04-16 05:45:15 +0000

    Fixing typos.

--- build-xpi.sh
+++ build-xpi.sh
@@ -1,11 +1,11 @@
 rm -rf .xpi_work_dir/
 rm -f opmlsupport.xpi
 mkdir .xpi_work_dir
-cp -r opmlsupport-new/* .xpi_work_dir/
+cp -r opmlsupport/* .xpi_work_dir/
 cd .xpi_work_dir/
 rm -rf `find . -name ".git"`
 rm -rf `find . -name ".DS_Store"`
 rm -rf `find . -name "Thumbs.db"`
-zip -rq ~/Desktop/opmlsupport-new.xpi *
+zip -rq ~/Desktop/opmlsupport.xpi *
 cd ..
 rm -rf .xpi_work_dir/
